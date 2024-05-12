dfd915d3362d93cda0eb996ef9872879aa6cdbee
Daksh Khandelwal <slay3rxo@gmail.com>
2024-05-11 22:22:08 -0400

    V12

--- src/app/components/Thanks.tsx
+++ src/app/components/Thanks.tsx
@@ -2,7 +2,7 @@ import Video from "./Video";
 
 export default function Thanks () {
     return (
-        <div className="w-screen h-screen overflow-hidden flex items-center justify-center bg-white dark:bg-black">
+        <div className="w-full h-screen overflow-hidden flex items-center justify-center bg-white dark:bg-black">
             <Video />
         </div>
     )
