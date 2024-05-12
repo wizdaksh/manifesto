915011f6d8eef6c99e28d22d1db6385f7855abfa
Daksh Khandelwal <slay3rxo@gmail.com>
2024-05-11 22:10:23 -0400

    V10

--- src/app/components/Thanks.tsx
+++ src/app/components/Thanks.tsx
@@ -4,9 +4,6 @@ export default function Thanks () {
     return (
         <div className="w-screen h-screen overflow-hidden flex items-center justify-center bg-white dark:bg-black">
             <Video />
-            <div className="absolute left-1/2 top-1/2 translate-x-1/2 translate-y-1/2">
-                <h1 className="text-black dark:text-white">Thank you!</h1>
-            </div>
         </div>
     )
 }
